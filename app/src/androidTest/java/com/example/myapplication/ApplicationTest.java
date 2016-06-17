c64ed0a4af0f89f4cfeaaf5a73eaa971bf8c23dc
Антон <p13lav@yandex.ru>
2016-06-17 14:16:21 +0400

    Testing java commiting

--- app/src/androidTest/java/com/example/myapplication/ApplicationTest.java
+++ app/src/androidTest/java/com/example/myapplication/ApplicationTest.java
@@ -9,7 +9,7 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
-        System.out.println("тестинг");
+        System.out.println("1");
     }
 
 }
